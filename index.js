e983ef45e1b10492739db05c03ef5e50b1b9c799
Lible^Niel <daniel.yanuar@reebonz.com>
2020-08-31 00:17:44 +0700

    Fix typo and change method to async

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import { NativeModules, Platform } from 'react-native';
 const RCTAppleAuthentication = NativeModules.ReactNativeAppleAuthentication;
 
 export const ReactNativeAppleAuthentication = {
-  requestAppleAuthentication: function(callback) {
+  requestAppleAuthentication: async function(callBack) {
     if(Platform.OS === 'ios') {
       await RCTAppleAuthentication.requestAsync({
           requestedScopes: [RCTAppleAuthentication.Scope.FULL_NAME, RCTAppleAuthentication.Scope.EMAIL],
         }).then((response) => {
-          callBack(response) //Display response
-          }, (error) => {
-            callBack(error) //Display error
-          });
+            callBack(response) //Display response
+        }, (error) => {
+          callBack(error) //Display error
+        });
     } else {
       return null
     }
